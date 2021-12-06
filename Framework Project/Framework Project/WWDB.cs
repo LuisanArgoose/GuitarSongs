85e36533c0872355b655960e2c0c124f892ecb57
Gorynych1337 <25215voe@mail.ru>
2021-12-06 12:24:40 +0300

    New .NET Fraimwork project

---
+++ Framework Project/Framework Project/WWDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Project_A
+{
+    public class WWDB
+    {
+        string pg_sign_data = "Host=localhost;Username=postgres;Password=admin123;Database=GuitarLessons";
+        private NpgsqlConnection con;
+
+        public WWDB()
+        {
+            this.con = new NpgsqlConnection(pg_sign_data);
+            this.con.Open();
+        }
+        public String get_version()
+        {
+            string com = "SELECT version()";
+            var cmd = new NpgsqlCommand(com, this.con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            return $"PostgreSQL version: {version}";
+        }
+    }
+}
